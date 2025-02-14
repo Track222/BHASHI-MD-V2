5282763f57e35564228cffed1ae0ce9cc3297012
Track222 <gimshandilisha400@gmail.com>
2025-02-14 20:50:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "BHASHI-MD~bFgW0ayQ#zBDCq9_moB2Opm01-HsYEJ-fGLN_WO5Evgwhn-NVeJ0",
 // For extra thing : BHASHI-MD~(megafileid)
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:mkNXYJgXRnMXvdmeNhOPaAMVbZUQiLCa@roundhouse.proxy.rlwy.net:57267",
 
     
  
